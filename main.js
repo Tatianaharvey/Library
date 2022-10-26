31e8682dd7628809be270aabca61d7b7e32a9784
Tatiana <110640811+Tatianaharvey@users.noreply.github.com>
2022-10-26 10:20:53 -0700

    add js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const addBook = document.getElementById('add-book')
-
 let myLibrary = [];
 
 class Book {
@@ -19,39 +17,81 @@ function addBookToLibrary(title, author, pages, status) {
 }
 
 
- 
+function showLibraryInfo() {
+    const booksRead = document.querySelector('#books-read');
+    const booksUnread = document.querySelector('#books-unread');
+    const totalBooks = document.querySelector('#total-books');
+    let readCounter = 0;
+    let unreadCounter = 0;
+    booksRead.textContent = 0;
+    booksUnread.textContent = 0;
+    for (let i = 0; i < myLibrary.length; i += 1) {
+      if (myLibrary[i].status === true) {
+        readCounter += 1;
+        booksRead.textContent = readCounter;
+      } else if (myLibrary[i].status === false) {
+        unreadCounter += 1;
+        booksUnread.textContent = unreadCounter;
+      }
+    }
+    totalBooks.textContent = myLibrary.length;
+  }
+
+
+function showBooksInLibrary() {
+    showLibraryInfo();
+    const bookList = document.querySelector('#table-body');
+    bookList.textContent = '';
+    for (let i = 0; i < myLibrary.length; i += 1) {
+        const bookRow = document.createElement('tr');
+        bookRow.classList.add('book-info');
+        bookList.appendChild(bookRow);
+
+        const bookTitle = document.createElement('td');
+        bookTitle.textContent = myLibrary[i].title;
+        bookRow.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement('td');
+        bookAuthor.textContent = myLibrary[i].author;
+        bookRow.appendChild(bookAuthor);
+
+        const bookPages = document.createElement('td');
+        bookPages.textContent = myLibrary[i].pages;
+        bookRow.appendChild(bookPages);
+
+        const bookStatus = document.createElement('td');
+        const readButton = document.createElement('BUTTON');
+        const readBook = document.createTextNode("read");
+        readButton.appendChild(readBook);
+        const unreadButton = document.createElement('BUTTON');
+        const unreadBook = document.createTextNode("unread");
+        unreadButton.appendChild(unreadBook);  
+        if (myLibrary[i].status === false) {
+            unreadButton.appendChild(bookStatus);
+        } else {
+            readButton.appendChild(bookStatus);
+        }
+        bookRow.appendChild(bookStatus);
+
+        const bookDelete = document.createElement('td');
+        const deleteButton = document.createElement('BUTTON');
+        const removeBook = document.appendTextNode("remove")
+        deleteButton.appendChild(removeBook);
+        bookDelete.appendChild(deleteButton);
+    }
+}
+
 
 function validateForm(event) {
     event.preventDefault();
     const form = document.querySelector('form');
-    const titleInput = document.querySelector('#title')
-    const titleError = document.querySelector('.title')
-    const authorInput = document.querySelector('#author')
-    const authorError = document.querySelector('.author')
-    const pagesInput = document.querySelector('#pages')
-    const pagesError = document.querySelector('.pages')
-    const checkbox = document.querySelector('input[name="checkbox"]')
-
-    if (titleInput.value === '') {
-        titleError.style.display = 'block';
-    } else {
-        titleError.style.display = 'none';
-    }
-
-    if (authorInput.value === '') {
-        authorError.style.display = 'block';
-    } else {
-        authorError.style.display = 'none';
-    }
-
-    if (pagesInput.value === '' || pagesInput.value.match(/[^1-9]/) || pagesInput.value <= 0) {
-        pagesError.style.display = 'block';
-    } else {
-        pagesError.style.display = 'none';
-    }
-
-    if (titleInput.value !== '' && authorInput.value !== '' && pagesInput.value !== '' && pagesInput.value > 0) {
-        if (checkbox) {
+    const titleInput = document.querySelector('#title');
+    const authorInput = document.querySelector('#author');
+    const pagesInput = document.querySelector('#pages');
+    const statusInput = document.querySelector('input[name="checkbox"]');
+    
+    if (titleInput.value !== '' && authorInput.value !== '' && pagesInput.value >= 0) {
+        if(checkbox.checked) {
             addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, true);
         } else {
             addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, false);
@@ -60,38 +100,18 @@ function validateForm(event) {
     }
 }
 
-function showBooksInLibrary() {
-const bookList = document.querySelector('#table-body');
-  bookList.textContent = '';
-  for (let i = 0; i < myLibrary.length; i += 1) {
-    const bookRow = document.createElement('tr');
-    bookRow.classList.add('book-info');
-    bookList.appendChild(bookRow);
-
-    const bookTitle = document.createElement('td');
-    bookTitle.textContent = myLibrary[i].title;
-    bookRow.appendChild(bookTitle);
-    
-    const bookAuthor = document.createElement('td');
-    bookAuthor.textContent = myLibrary[i].author;
-    bookRow.appendChild(bookAuthor);
-    
-    const bookPages = document.createElement('td');
-    bookPages.textContent = myLibrary[i].pages;
-    bookRow.appendChild(bookPages);
-  }
-}
-
 function listenClicks() {
     document.addEventListener('click', (event) => {
-        
         const { target } = event;
         const tr = target.parentNode.parentNode.rowIndex - 1;
         if (target.id === 'add-book') {
-        validateForm(event);
-        console.log(myLibrary)
-        } else {
-
+            validateForm(event);
+        } else if (target.id === 'deleteButton') {
+            myLibrary.splice(tr, 1);
+        } else if (target.id === 'unreadButton') {
+            myLibrary[tr].status = false;
+        } else if (target.id === 'readButton') {
+            myLibrary[tr].status = true;
         }
         showBooksInLibrary();
     })
@@ -100,8 +120,7 @@ function listenClicks() {
 showBooksInLibrary();
 listenClicks();
 
-console.log(listenClicks)
-console.log(showBooksInLibrary)
+
 
 
 // Popup form 
@@ -109,6 +128,7 @@ console.log(showBooksInLibrary)
 const modal = document.querySelector(".modal");
 const openForm = document.querySelector(".openForm");
 const closeButton = document.querySelector(".close");
+const addAndClose = document.querySelector('.add-and-close')
 
 function toggleModal() {
     modal.classList.toggle("show-modal");
@@ -123,3 +143,4 @@ function windowOnClick(event) {
 openForm.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+addAndClose.addEventListener("click", toggleModal);
